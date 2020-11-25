b6c9ab53e051c0a3b70d36b2bad1fb7dd49c6383
Alain Landry <alain.landry@canac.ca>
2020-11-25 00:03:33 -0500

    Lesson 21: Adding logic to switch between task statuses with Knockout data-bind attribute- NO WORK FOR ME

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,20 +1,36 @@
-define(['uiComponent'], function (Component) {
+define([
+    'uiComponent',
+    'jquery'
+], function (Component, $) {
     'use strict';
 
     return Component.extend({
         defaults: {
             tasks: [
-                {label: "Task 1"},
-                {label: "Task 2"},
-                {label: "Task 3"},
-                {label: "Task 4"},
-            ]
+                {id: 1, label: "Task 1", status: true},
+                {id: 2, label: "Task 2", status: true},
+                {id: 3, label: "Task 3", status: false},
+                {id: 4, label: "Task 4", status: true},
+            ],
         },
+
         initObservable: function () {
             this._super().observe(['tasks']);
-            this.tasks().push({label: 'Task 5'});
 
             return this;
+        },
+
+        switchStatus: function (data, event) {
+            const taskId = $(event.target).data('id');
+
+            var items = this.tasks().map(function (task) {
+                if (task.id === taskId) {
+                    task.status = !task.status;
+                }
+                return task
+            });
+
+            this.tasks(items);
         },
     });
 });
