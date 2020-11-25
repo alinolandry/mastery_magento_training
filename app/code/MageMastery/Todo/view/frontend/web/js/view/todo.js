c92e3bb94a8268d413ba078db2dd31bd5ebe150e
Alain Landry <alain.landry@canac.ca>
2020-11-24 21:39:51 -0500

    Lesson 16: Adding tasks to the UI Component defaults

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,4 +1,14 @@
 define(['uiComponent'], function (Component) {
     'use strict';
-    return Component.extend({});
+
+    return Component.extend({
+        defaults: {
+            tasks: [
+                {label: "Task 1"},
+                {label: "Task 2"},
+                {label: "Task 3"},
+                {label: "Task 4"},
+            ]
+        }
+    });
 });
