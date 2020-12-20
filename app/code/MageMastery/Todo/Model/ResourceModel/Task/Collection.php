734ac71c4c309470aa08b306afaeb017f12400d8
Alain Landry <alain.landry@canac.ca>
2020-12-20 11:30:18 -0500

    Task: Lesson 37: Implementing Search Results interface for Task Collection class

--- app/code/MageMastery/Todo/Model/ResourceModel/Task/Collection.php
+++ app/code/MageMastery/Todo/Model/ResourceModel/Task/Collection.php
@@ -4,15 +4,82 @@
 namespace MageMastery\Todo\Model\ResourceModel\Task;
 
 
+use MageMastery\Todo\Api\Data\TaskInterface;
+use MageMastery\Todo\Api\Data\TaskSearchResultInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\Api\SearchResultsInterface;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use MageMastery\Todo\Model\Task;
 use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
 
-class Collection extends AbstractCollection
+class Collection extends AbstractCollection implements TaskSearchResultInterface
 {
+    /**
+     * @var SearchCriteriaInterface
+     */
+    private $searchCriteria;
+
     protected function _construct()
     {
         $this->_init(Task::class, TaskResource::class);
     }
 
+    /**
+     * Get search criteria.
+     *
+     * @return \Magento\Framework\Api\SearchCriteriaInterface
+     */
+    public function getSearchCriteria()
+    {
+        return $this->searchCriteria;
+    }
+
+    /**
+     * Set search criteria.
+     *
+     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     * @return $this
+     */
+    public function setSearchCriteria(SearchCriteriaInterface $searchCriteria)
+    {
+        $this->searchCriteria = $searchCriteria;
+        return $this;
+    }
+
+
+    /**
+     * Get total count.
+     *
+     * @return int
+     */
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+
+    /**
+     * Set total count.
+     *
+     * @param int $totalCount
+     * @return $this
+     */
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+
+    /**
+     * @param array|null $items
+     * @return $this
+     */
+    public function setItems(array $items = null)
+    {
+        if(!$items){
+            return $this;
+        }
+        foreach ($items as $item){
+            $this->addItem($item);
+        }
+        return $this;
+    }
 }
